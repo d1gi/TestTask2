c3693bd87badd235a878a382cbb457a55e8d3f11
d1gi <digital_boy@mail.ru>
2015-08-23 22:01:07 +0600

    add Form methods

--- src/TestTask/Form/UserForm.php
+++ src/TestTask/Form/UserForm.php
@@ -2,7 +2,43 @@
 
 namespace TestTask\Form;
 
+use TestTask\Entity\User;
+
 class UserForm
 {
+    /**
+     * Приаязать выходные данные к сущности
+     */
+    public function bind()
+    {
 
+    }
+
+    /**
+     * Получить сущность
+     *
+     * @return User
+     */
+    protected function getData()
+    {
+
+    }
+
+    /**
+     * Получить сгенерированый шаблон формы.
+     */
+    public function getView()
+    {
+
+    }
+
+    /**
+     * Проверка на валидность
+     *
+     * @return bool
+     */
+    public function isValid()
+    {
+
+    }
 }
